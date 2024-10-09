f1bbad2305537bb600399558a0080ae08a75eaf7
aditya-mudili <adityamudili99@gmail.com>
2024-10-09 22:50:04 +0530

    tejaswini portfolio

---
+++ src/content_option.js
@@ -0,0 +1,120 @@
+const logotext = "SWETHA";
+const meta = {
+    title: "Tejaswini Sai Swetha",
+    description: "I’m Mudili Tejaswini Sai Swetha, 2023 passout",
+};
+
+const introdata = {
+    title: "I’m Tejaswini Sai Swetha",
+    animated: {
+        first: "I love coding",
+        second: "I develop mobile apps",
+        third: "I code cool websites",
+    },
+    description: "I am Btech graduate from Chaitanya institute of technology and science",
+    your_img_url: "https://images.unsplash.com/photo-1514790193030-c89d266d5a9d",
+};
+
+const dataabout = {
+    title: "a bit about my self",
+    aboutme: "I am entusiastist graduate ",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Android development",
+        value: 90,
+    },
+    {
+        name: "Web development",
+        value: 85,
+    },
+    {
+        name: "Javascript",
+        value: 80,
+    },
+    {
+        name: "React",
+        value: 60,
+    },
+    {
+        name: "Jquery",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://picsum.photos/400/?grayscale",
+        description: "This is a women safety android application which helps women who are in danger by sending SOS.",
+        link: "https://github.com/tejaswinisaiswetha-mudili/Stree-Raksha-app",
+    },
+    {
+        img: "https://picsum.photos/400/?grayscale",
+        description: "This is a mini android robot in which the robot does the human works. Code is in Kotlin.",
+        link: "https://github.com/tejaswinisaiswetha-mudili/Robot",
+    },
+    {
+        img: "https://picsum.photos/400/600/?grayscale",
+        description: "In this combining historical usage patterns with weather data in order to forecast hourly bike demand.",
+        link: "https://github.com/tejaswinisaiswetha-mudili/HourlyBikeRental",
+    },
+    {
+        img: "https://picsum.photos/400/300/?grayscale",
+        description: "This is to predict if the client will subscribe to term deposit or not.",
+        link: "https://github.com/tejaswinisaiswetha-mudili/Deposit-Prediction",
+    },
+    
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "swethamudili99@gmail.com",
+    YOUR_FONE: "9959171595",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+};
+
+const socialprofils = {
+    github: "https://github.com/tejaswinisaiswetha-mudili/",
+    facebook: "https://www.facebook.com/swetha.mudili.1",
+    linkedin: "https://www.linkedin.com/in/tejaswini-sai-swetha-mudili-42948a216/",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
---
+++ src/pages/contact/index.js
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import * as emailjs from "emailjs-com";
+import "./style.css";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+import { meta } from "../../content_option";
+import { Container, Row, Col, Alert } from "react-bootstrap";
+import { contactConfig } from "../../content_option";
+
+export const ContactUs = () => {
+  const [formData, setFormdata] = useState({
+    email: "",
+    name: "",
+    message: "",
+    loading: false,
+    show: false,
+    alertmessage: "",
+    variant: "",
+  });
+
+  return (
+    <HelmetProvider>
+      <Container>
+        <Helmet>
+          <meta charSet="utf-8" />
+          <title>{meta.title} | Contact</title>
+          <meta name="description" content={meta.description} />
+        </Helmet>
+        <Row className="mb-5 mt-3 pt-md-3">
+          <Col lg="8">
+            <h1 className="display-4 mb-4">Contact Me</h1>
+            <hr className="t_border my-4 ml-0 text-left" />
+          </Col>
+        </Row>
+        <Row className="sec_sp">
+          <Col lg="12">
+            <Alert
+              //show={formData.show}
+              variant={formData.variant}
+              className={`rounded-0 co_alert ${
+                formData.show ? "d-block" : "d-none"
+              }`}
+              onClose={() => setFormdata({ show: false })}
+              dismissible
+            >
+              <p className="my-0">{formData.alertmessage}</p>
+            </Alert>
+          </Col>
+          <Col lg="5" className="mb-5">
+            <h3 className="color_sec py-4">Get in touch</h3>
+            <address>
+              <strong>Email:</strong>{" "}
+              <a href={`mailto:${contactConfig.YOUR_EMAIL}`}>
+                {contactConfig.YOUR_EMAIL}
+              </a>
+              <br />
+              <br />
+              {contactConfig.hasOwnProperty("YOUR_FONE") ? (
+                <p>
+                  <strong>Phone:</strong> {contactConfig.YOUR_FONE}
+                </p>
+              ) : (
+                ""
+              )}
+            </address>
+            <p>{contactConfig.description}</p>
+          </Col>
+          <Col lg="7" className="d-flex align-items-center">
+            <img src="https://passporttoshine.com/wp-content/uploads/2023/05/Contact-me-1920x1080.jpg"/>
+          </Col>
+        </Row>
+      </Container>
+      <div className={formData.loading ? "loading-bar" : "d-none"}></div>
+    </HelmetProvider>
+  );
+};
